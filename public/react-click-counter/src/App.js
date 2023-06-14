417f9717de96f61c637698c1ab0f8bcc1161bb78
elmaranchuelo <elmaranchuelo@gmail.com>
2023-06-14 23:33:23 +0800

    10th commit - Updated app.js and added Click functions

--- public/react-click-counter/src/App.js
+++ public/react-click-counter/src/App.js
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
+
 
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/clicks')
+      .then(response => response.json())
+      .then(data => setCount(data.count))
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleClick = () => {
+    fetch('http://127.0.0.1:8000/api/clicks', { method: 'POST' })
+      .then(response => response.json())
+      .then(data => setCount(data.count))
+      .catch(error => console.error(error));
+  };
+
   return (
-    <div>
+    <div className="container">
       <h1>Click Counter</h1>
-      <p>Number of clicks: {count}</p>
-      <button>Click Me!</button>
+      <p className="click-counter">Number of clicks: {count}</p>
+      <div className="button-container">
+        <button className="button" onClick={handleClick}>
+          Click Me!
+        </button>
+      </div>
     </div>
   );
 }
